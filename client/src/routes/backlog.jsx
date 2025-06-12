96bebf67e4c711876ee1bb94260f012b18aa9072
Maarten Ighade <ighade.maarten@gmail.com>
2025-06-12 11:13:03 +0200

    online fixes

--- client/src/routes/backlog.jsx
+++ client/src/routes/backlog.jsx
@@ -1,6 +1,6 @@
 import { createRoute } from '@tanstack/react-router';
 import { Route as RootRoute } from './__root';
-import Backlog from '../pages/PaginatedBacklog/backlog';
+import Backlog from '../pages/PaginatedBacklog/Backlog';
 
 export const Route = createRoute({
   getParentRoute: () => RootRoute,
